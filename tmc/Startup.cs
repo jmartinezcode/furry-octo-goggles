93e3bfdd334ceeec7a41df44191ea168ba16cf7d
Jacob Martinez <jmartinez.data@gmail.com>
2020-03-26 16:22:13 -0500

    Added files for materialize for future implementation, removed roles from registration

--- tmc/Startup.cs
+++ tmc/Startup.cs
@@ -32,7 +32,7 @@ namespace tmc
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
